d074e46b807ad2c4bc65a7ed03c8aacee09c184d
Delvechio Duarte <contato@delvechiodev.com>
2023-03-04 10:52:26 -0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char[][] JogoVelha = new char[3][3];
+
+    }
+}
