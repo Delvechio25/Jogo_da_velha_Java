ced295ac92ccaeeb2756fe43434b495af8564681
Delvechio Duarte <contato@delvechiodev.com>
2023-03-04 11:22:33 -0300

    Validation if the row and column are valid

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        char[][] JogoVelha = new char[3][3];
+        char[][] hash = new char[3][3];
+        System.out.println("Player 1 = X");
+        System.out.println("Player 2 = O");
+
+        boolean win = false;
+        int move = 1;
+        char sinal;
+        int row = 0, column = 0;
+
+        while (!win){
+
+            if (move % 2 == 1){ //player 1
+                System.out.println("player 1 do your move. Choose row and a column");
+                sinal = 'X';
+            }else {
+                System.out.println("player 2 do your move. Choose row and a column");
+                sinal = 'O';
+            }
+
+            boolean rightRow = false;
+            while (!rightRow){
+                System.out.println("Iput the row (1,2,3) ");
+                row = scanner.nextInt();
+                if (row >= 1 && row <=3){
+                    rightRow = true;
+                }
+                else {
+                    System.out.println("Try again invalid row");
+                }
+            }
+
+        }
 
     }
 }
