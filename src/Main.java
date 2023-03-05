9e7a36c2fd6548da5fd57596a29e587309e852e5
Delvechio Duarte <contato@delvechiodev.com>
2023-03-05 09:11:20 -0300

    Project has finished

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -25,7 +26,7 @@ public class Main {
 
             boolean validRow = false;
             while (!validRow){
-                System.out.println("Iput the row (1,2,3). ");
+                System.out.println("Iput the row (0, 1, 2). ");
                 row = scanner.nextInt();
                 if (row >= 1 && row <=3){
                     validRow = true;
@@ -36,8 +37,8 @@ public class Main {
             }
 
             boolean validcolumn = true;
-            while (!!validcolumn){
-                System.out.println("input the column (1, 2, 3). ");
+            while (!validcolumn){
+                System.out.println("input the column (0, 1, 2). ");
                 column = scanner.nextInt();
                 if (column >= 1 && column <=3){
                     validcolumn = true;
@@ -46,8 +47,8 @@ public class Main {
                     System.out.println("Try again invalid column.");
                 }
             }
-            row--;
-            column--;
+            //row--;
+            //column--;
             if (hash[row][column] == 'X' || hash[row][column] == 'O'){
                 System.out.println("Try another position");
             }else { // jogada valida
@@ -56,8 +57,33 @@ public class Main {
             }
             for (int i = 0; i <hash.length; i++){
                 for (int j = 0;j < hash[i].length; j++){
-
+                    System.out.print(hash[i][j] + " | ");
                 }
+                System.out.println();
+            }
+            if ((hash[0][0] == 'X' && hash[0][1] == 'X' && hash[0][2] == 'X') || // row 1
+                    (hash[1][0] == 'X' && hash[1][1] == 'X' && hash[1][2] == 'X')|| // row 2
+                    (hash[2][0] == 'X' && hash[2][1] == 'X' && hash[2][2] == 'X')|| // row 3
+                    (hash[0][0] == 'X' && hash[1][1] == 'X' && hash[2][2] == 'X')|| //column 1
+                    (hash[0][0] == 'X' && hash[0][1] == 'X' && hash[0][2] == 'X')|| //column 1
+                    (hash[0][1] == 'X' && hash[0][1] == 'X' && hash[0][1] == 'X')|| //column 1
+                    (hash[0][0] == 'X' && hash[1][1] == 'X' && hash[2][2] == 'X')){ // diagonal
+                win = true;
+                System.out.println("Congrats. Player one is the winner");
+
+            } else if ((hash[0][0] == 'O' && hash[0][1] == 'O' && hash[0][2] == 'O') || // row 1
+                    (hash[1][0] == 'O' && hash[1][1] == 'O' && hash[1][2] == 'O')|| // row 2
+                    (hash[2][0] == 'O' && hash[2][1] == 'O' && hash[2][2] == 'O')|| // row 3
+                    (hash[0][0] == 'O' && hash[1][1] == 'O' && hash[2][2] == 'O')|| //column 1
+                    (hash[0][0] == 'O' && hash[0][1] == 'O' && hash[0][2] == 'O')|| //column 1
+                    (hash[0][1] == 'O' && hash[0][1] == 'O' && hash[0][1] == 'O')|| //column 1
+                    (hash[0][0] == 'O' && hash[1][1] == 'O' && hash[2][2] == 'O')) {
+                win = true;
+                System.out.println("Congrats. Player two is the winner");
+                
+            }else if (move > 9){
+                win = true;
+                System.out.println("Anyone won the game.");
             }
 
         }
